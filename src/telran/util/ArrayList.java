89c935cb0f6f3ba27df2b0f5cff4584a11177316
vorabkin <vorabkin@gmail.com>
2021-11-05 14:06:21 +0200

    adds removeIf() refactoring, sortedSearch() & clear()

--- src/telran/util/ArrayList.java
+++ src/telran/util/ArrayList.java
@@ -112,21 +112,66 @@ public class ArrayList<T> implements List<T> {
 	@Override
 	public boolean removeIf(Predicate<T> predicate) {
 		//O[N^2]
+		/*
 		int oldSize = size;
 		for (int i = size - 1; i >= 0; i--) {
 			if (predicate.test(array[i])) {
 				remove(i);
 			}
 		}
-		
 		return oldSize > size;
+		*/
 		//TODO rewrite the method for O[N] complexity
+		
+		int oldSize = size;
+		int indexCopy = 0;
+		for(int index=0; index<oldSize; index++) {
+			if(!predicate.test(array[index])) {
+				array[indexCopy++] = array[index];
+			}
+		}
+		size = indexCopy;
+		return oldSize > size;
+		
+		// Done
 	}
 	@Override
 	public void sort(Comparator<T> comp) {
 		//O[N * LogN]
 		Arrays.sort(array, 0, size, comp);
-		
+	
+	}
+	@Override
+	public int sortedSearch(T pattern, Comparator<T> comp) {
+		// TODO Auto-generated method stub
+		// implied that array is sorted in accordance with given comparator
+		int left = 0;
+		int right = size -1;
+		int res = -1;
+		while (left <= right) {
+			int middle = (left + right) / 2;
+			int resComp = comp.compare(pattern, array[middle]);
+			if(resComp == 0) {
+				res = middle;
+				break;
+			}
+			if(resComp > 0) {
+				left = middle + 1;
+				res = -(middle + 2);
+			} else {
+				right = middle - 1;
+				res = -(middle + 1);
+			}
+		}
+		return res;
+		// Done
+	}
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		array = (T[]) new Object[array.length];
+		size = 0;
+		// Done
 	}
 	
 
